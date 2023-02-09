7c7644f3e1d5cd8dcf65dde58c568e4beec5262a
Adesh Singh Rajawat <aadeshsingh1947@gmail.com>
2023-02-10 00:12:29 +0530

    reactive-form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-    return <form>
+    const [enteredTitle,setEnteredTitle]= useState('');
+    const [enteredAmount,setEnteredAmount]= useState('');
+    const [enteredDate,setEnteredDate]= useState('');
+
+    const titleChangeHandler=(event)=>{
+        setEnteredTitle(event.target.value);
+        // console.log(event.target.value)
+    }
+        const amountChangeHandler=(event)=>{
+            setEnteredAmount(event.target.value);
+    }
+    const dateChangeHandler=(event)=>{
+        setEnteredDate(event.target.value);
+}
+    const submitHandler=(event)=>{
+        event.preventDefault();
+
+        const ExpenseData ={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate)
+        }
+        console.log(ExpenseData)
+    }
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text"></input>
+            <input type="text" onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
             <label>amount</label>
-            <input type="number" min="0.01" step="0.01"></input>
+            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}></input>
         </div>
         <div className="new-expense__control">
             <label>date</label>
-            <input type="date" min="2021-01-01"></input>
+            <input type="date" min="2021-01-01" onChange={dateChangeHandler}></input>
         </div>
           <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
