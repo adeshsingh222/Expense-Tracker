c4e1d7b18d00f2dd2dcedeadbb969d631ce17d11
Adesh Singh <aadeshsingh1947@gmail.com>
2023-02-18 15:40:15 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
+import React, { useState } from 'react';
 
-import Expenses from "./components/Expenses/Expenses";
-import NewExpenses from "./components/NewExpenses/NewExpenses";
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Tooth Brush",
-      amount: 23,
-      date: new Date(2020, 3, 23),
-    },
-    {
-      id: "e2",
-      title: "Comic",
-      amount: 299,
-      date: new Date(2022, 2, 3),
-    },
-    {
-      id: "e3",
-      title: "Bread",
-      amount: 3,
-      date: new Date(2021, 8, 29),
-    },
-    {
-      id: "e4",
-      title: "Beans",
-      amount: 10,
-      date: new Date(2021, 1, 12),
-    },
-  ];
+import NewExpense from './components/NewExpenses/NewExpenses';
+import Expenses from './components/Expenses/Expenses';
 
-  const addNewExpenseHandler= expense =>{
-     console.log('in-App')
-     console.log(expense)
-  }
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+  
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-    <div >
-      <NewExpenses onAddNewExpense={addNewExpenseHandler}/>
-      <Expenses items={expenses}/>
-     
+    <div>
+      
+      <NewExpense onAddExpense={addExpenseHandler} />
+    
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,18 +1,32 @@
-import React from "react";
-import "./NewExpenses.css"
-import ExpenseForm from "./ExpenseForm";
+import React, {useState} from 'react';
 
- const NewExpenses= (props) => {
-    const saveExpenseDataHandler=(enteredExpenseData)=>{
-        const expenseData={
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-       props.onAddNewExpense(expenseData);
-    }
-    return <div className="new-expense">
-        <ExpenseForm onSaveExpenseHandler={saveExpenseDataHandler}/>
+import ExpenseForm from './ExpenseForm';
+import './NewExpenses.css';
+
+const NewExpense = (props) => {
+const [isEditing,setIsEditing]= useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+     const StartEditing= ()=>{
+      setIsEditing(true);
+     }
+   
+     const stopEditingHandler=()=>{
+      setIsEditing(false);
+     }
+
+  return (
+    <div className='new-expense'>
+        { !isEditing &&<button onClick={StartEditing}>Add new Expenses</button>}
+     { isEditing&&<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancle={stopEditingHandler}/>}
     </div>
-}
+  );
+};
 
-export default NewExpenses;
+export default NewExpense;
